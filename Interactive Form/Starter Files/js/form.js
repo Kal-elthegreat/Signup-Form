cb1d4a61fbc1b9622f7ee769425da02f0933ec4d
Kiel Lewis <kiel.lewis@gmail.com>
2018-12-19 10:45:25 -0500

    added css & form validation progress
    > added css changes to checkbox conditionals
    
    > form validation in progress, regex.test() completed for each input field

--- Interactive Form/Starter Files/js/form.js
+++ Interactive Form/Starter Files/js/form.js
@@ -67,34 +67,62 @@ $('.activities input').each(function(index){
     
     if($('.activities input').eq(1).prop('checked')){
         $('.activities input').eq(3).attr('disabled', true);
+        $('.activities label').eq(3).css('color', 'grey');
+
         $('.activities input').eq(5).attr('disabled', true);
+        $('.activities label').eq(5).css('color', 'grey');
+
           
     } else if ($('.activities input').eq(3).prop('checked')){
         $('.activities input').eq(1).attr('disabled', true);
+        $('.activities label').eq(1).css('color', 'grey');
+
         $('.activities input').eq(5).attr('disabled', true);
+        $('.activities label').eq(5).css('color', 'grey');
+
 
     } else if($('.activities input').eq(5).prop('checked')){
         $('.activities input').eq(3).attr('disabled', true);
+        $('.activities label').eq(3).css('color', 'grey');
+
         $('.activities input').eq(1).attr('disabled', true);
+        $('.activities label').eq(1).css('color', 'grey');
+
 
     } else {
         $('.activities input:odd').attr('disabled', false)
+        $('.activities label:odd').css('color', 'black');
+
 
     }
     if($('.activities input').eq(2).prop('checked')){
         $('.activities input').eq(4).attr('disabled', true);
+        $('.activities label').eq(4).css('color', 'grey');
+
         $('.activities input').eq(6).attr('disabled', true);
+        $('.activities label').eq(6).css('color', 'grey');
+
         
     } else if ($('.activities input').eq(4).prop('checked')){
         $('.activities input').eq(2).attr('disabled', true);
+        $('.activities label').eq(2).css('color', 'grey');
+
         $('.activities input').eq(6).attr('disabled', true);
+        $('.activities label').eq(6).css('color', 'grey');
+
 
     } else if($('.activities input').eq(6).prop('checked')){
         $('.activities input').eq(2).attr('disabled', true);
+        $('.activities label').eq(2).css('color', 'grey');
+
         $('.activities input').eq(4).attr('disabled', true);
+        $('.activities label').eq(4).css('color', 'grey');
+
 
     } else {
         $('.activities input:even').attr('disabled', false)
+        $('.activities label:even').css('color', 'black');
+
 
     }
     
@@ -132,54 +160,44 @@ $('#payment').on('change',function(){
 
 
 
+/************************ Form Validation *****************/
+const userName = document.getElementById('name');
+const userEmail = document.getElementById('mail');
+const userCC = document.getElementById('cc-num');
+const userZip = document.getElementById('zip');
+const userCVV = document.getElementById('cvv');
+
+function isValidName (userName){
+    return /^[A-Za-z]*\s?[A-Za-z]+$/.test($('input#name').val()) // test sname field
+}
+
+function isValidEmail (userEmail){
+    return  /^[^@]+@[^\.]+\.[a-z]+$/i.test($('input#mail').val()) // test email field
+}
+
+function isValidCC (userCC){
+   return /^\d{13,16}$/.test($('input#cc-num').val()) // test cc field
+}
+
+function isValidZip(userZip){
+    return /^\d{5}$/.test($('input#zip').val()) // test zip field
+}
+
+function isValidCVV (userCVV){
+   return /^\d{3}$/.test($('input#cvv').val()) // test cvv field
+}
+
+userName.addEventListener('input',(isValidName)=>{
+
+})
+
+userEmail.addEventListener('input',(isValidEmail) => {
+    
+})
+
+userCC.addEventListener('input',(isValidCC) =>{
+
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const regexName = /^[a-z\s]+/i  (name field regex)
-//const regexEmail = /^[^@]+@[^\.]+\.[a-z]+$/i (email field regex)
+    
